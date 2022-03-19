dd8d6a79269bc7f83e46f3ae54d22c2771b1b193
Laura Santana <00335259@ufrgs.br>
2022-03-19 06:01:48 -0300

    Alterações

--- Código/Codigo-Hacka.c
+++ Código/Codigo-Hacka.c
@@ -2,21 +2,56 @@
 #include <stdlib.h>
 #include <raylib.h>
 
+typedef struct linguas{
+
+    //Aqui o jogador pode definir se ele prefere que o jogo apareça em FR ou em PT.    
+    int Preferencia_PTouFR;
+
+    //Aqui o jogador pode definir se ele é proficiente em outras línguas, de bás-int-avç definindo um valor de 0-2.
+    int ProficienciaPT;
+        int visivelPT;
+    int ProficienciaFR;
+        int visivelFR;
+    int ProficienciaEN;
+        int visivelEN;
+    int ProficienciaSP;
+        int visivelSP;
+
+    //Opção de personalização, caso o jogador decida adicionar mais línguas a seu perfil.
+    char Outros1[10];
+    int ProficienciaO1;
+        int visivelO1;
+    char Outros2[10];
+    int ProficienciaO2;
+        int visivelO2;
+
+} Linguas;
+
 typedef struct player{
 
-    //Personalizáveis: nome, idade, aparência, gostos...
+    //Personalizáveis: nome, idade, aparência, bio...
     char Nome[20];
     int idade;
-    //Ideias: línguas faladas pelo jogador
+    Linguas Linguagens;
+    char Bio[300];
 
     //Características inerentes a ele
     int Atravessavel;
-    int Amigos;
+    Amigos ListaAmigos;
     int Nível;
 
 } Player;
 
-typedef struct amigos{}
+typedef struct amigos{
+    //Fazer como Lista Duplamente Encadeada.
+    //Inicializar função só para isso.
+
+    int quantidade;
+    Amigos* primeiro;
+    Amigos* prox;
+    Amigos* ant;
+
+} Amigos;
 
 int main (){
 
@@ -25,7 +60,7 @@ int main (){
     Player Jogador;
 
     Jogador.Atravessavel = 0;
-    Jogador.Amigos = 0;
+    //Função lista de amigos;
     Jogador.Nível = 0;
 
     initWindow(screenWidth, screenHeight, "Hackathon Enchante{é}");
@@ -35,5 +70,5 @@ int main (){
     initDrawing();
     endDrawing();
 
-
+    return 0;
 }
