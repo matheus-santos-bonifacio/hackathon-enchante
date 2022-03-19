7cae232ce54c37b28826b14e62f0dc4335829348
Laura Santana <00335259@ufrgs.br>
2022-03-19 06:20:31 -0300

    Update Codigo-Hacka.c

--- Código/Codigo-Hacka.c
+++ Código/Codigo-Hacka.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 #include <raylib.h>
 
 typedef struct linguas{
@@ -27,21 +28,6 @@ typedef struct linguas{
 
 } Linguas;
 
-typedef struct player{
-
-    //Personalizáveis: nome, idade, aparência, bio...
-    char Nome[20];
-    int idade;
-    Linguas Linguagens;
-    char Bio[300];
-
-    //Características inerentes a ele
-    int Atravessavel;
-    Amigos ListaAmigos;
-    int Nível;
-
-} Player;
-
 typedef struct amigos{
     //Fazer como Lista Duplamente Encadeada.
     //Inicializar função só para isso.
@@ -53,19 +39,68 @@ typedef struct amigos{
 
 } Amigos;
 
+typedef struct player{
+
+    //Personalizáveis: nome, idade, aparência, bio...
+    char Nome[20];
+    int Idade;
+    Linguas Linguagens;
+    char Bio[300];
+
+    //Características inerentes a ele
+    int Atravessavel;
+    Amigos ListaAmigos;
+    int Nível;
+
+} Player; //Informações provavelmente serão carregadas por meio de um arquivo texto durante o login.
+
+//FUNÇÕES
+
+int PersonalizaPlayer(Player* Jogador){
+
+    FILE *Arq;
+
+    if (Arq == NULL){
+
+        Arq = fopen("player.c", "w");
+
+        printf("Escolha um nome de exibição.\n");
+        getchar();
+	    fgets(Jogador->Nome, 20, stdin);
+
+        printf("Idade\n");
+        scanf("%d", &Jogador->Idade);
+
+        printf("Quais línguas você fala?\n");
+        //Função de linguagens;
+
+        printf("Conte um pouco sobre você! Insira uma bio.\n");
+        //Opcional;
+        getchar();
+	    fgets(Jogador->Bio, 300, stdin);
+
+        
+
+
+
+    }
+
+}
+
+//SALAS
+
 int main (){
 
     int screenWidth = 1000;
     int screenHeight = 800;
     Player Jogador;
 
-    Jogador.Atravessavel = 0;
-    //Função lista de amigos;
-    Jogador.Nível = 0;
+    setlocale(LC_ALL, "Portuguese");
 
     initWindow(screenWidth, screenHeight, "Hackathon Enchante{é}");
     
     //ABAIXO: Tudo o que aparecerá na tela. A lógica do jogo em si deve estar estruturada acima!
+    //Fazer em função para ficar mais organizado.
 
     initDrawing();
     endDrawing();
