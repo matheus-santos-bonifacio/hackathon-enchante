00beec9aca1aa458d83600fbb6713d7c44205fc6
Laura Santana <00335259@ufrgs.br>
2022-03-20 11:46:40 -0300

    FINAL

--- Código/Codigo-Hacka.c
+++ Código/Codigo-Hacka.c
@@ -6,6 +6,7 @@
 //Bibliotecas externas!
 #include <raylib.h>
 #include <libwebsockets.h>
+#include <signal.h>
 
 #define SCREEN_WIDTH 1440
 #define SCREEN_HEIGHT 800
@@ -14,6 +15,9 @@
 #define LINHAS 10
 #define LARGURA_PIXEL 72
 #define ALTURA_PIXEL 44
+
+#define LWS_PLUGIN_STATIC
+#include "protocol_lws_minimal.c"
 
 //Organizar struct de objetos para interação;
 
@@ -854,11 +858,316 @@ void Door(Player* Jogador, int CurrentScreen, Vector2 Mouse, Cenario Sala[][800]
 
 }
 
+// FUNÇÕES DE SERVIDOR
+static struct lws_protocols protocols[] = {
+	{ "http", lws_callback_http_dummy, 0, 0, 0, NULL, 0},
+	LWS_PLUGIN_PROTOCOL_MINIMAL,
+	LWS_PROTOCOL_LIST_TERM
+};
+
+static const lws_retry_bo_t retry = {
+	.secs_since_valid_ping = 3,
+	.secs_since_valid_hangup = 10,
+};
+
+static int interrupted;
+
+static const struct lws_http_mount mount = {
+	/* .mount_next */		NULL,		/* linked-list "next" */
+	/* .mountpoint */		"/",		/* mountpoint URL */
+	/* .origin */			"./mount-origin",  /* serve from dir */
+	/* .def */			"index.html",	/* default filename */
+	/* .protocol */			NULL,
+	/* .cgienv */			NULL,
+	/* .extra_mimetypes */		NULL,
+	/* .interpret */		NULL,
+	/* .cgi_timeout */		0,
+	/* .cache_max_age */		0,
+	/* .auth_mask */		0,
+	/* .cache_reusable */		0,
+	/* .cache_revalidate */		0,
+	/* .cache_intermediaries */	0,
+	/* .cache_no */			0,
+	/* .origin_protocol */		LWSMPRO_FILE,	/* files in a dir */
+	/* .mountpoint_len */		1,		/* char count */
+	/* .basic_auth_login_file */	NULL,
+};
+
+#if defined(LWS_WITH_PLUGINS)
+/* if plugins enabled, only protocols explicitly named in pvo bind to vhost */
+static struct lws_protocol_vhost_options pvo = { NULL, NULL, "lws-minimal", "" };
+#endif
+
+void sigint_handler(int sig)
+{
+	interrupted = 1;
+}
+
+//FUNÇÕES CLIENTE
+
+/*
+ * This represents your object that "contains" the client connection and has
+ * the client connection bound to it
+ */
+
+static struct my_conn {
+	lws_sorted_usec_list_t	sul;	     /* schedule connection retry */
+	struct lws		*wsi;	     /* related wsi if any */
+	uint16_t		retry_count; /* count of consequetive retries */
+} mco;
+
+static struct lws_context *context;
+static int interrupted, port = 443, ssl_connection = LCCSCF_USE_SSL;
+static const char *server_address = "localhost",
+		  *pro = "dumb-increment-protocol";
+
+/*
+ * The retry and backoff policy we want to use for our client connections
+ */
+
+static const uint32_t backoff_ms[] = { 1000, 2000, 3000, 4000, 5000 };
+
+static const lws_retry_bo_t retry = {
+	.retry_ms_table			= backoff_ms,
+	.retry_ms_table_count		= LWS_ARRAY_SIZE(backoff_ms),
+	.conceal_count			= LWS_ARRAY_SIZE(backoff_ms),
+
+	.secs_since_valid_ping		= 3,  /* force PINGs after secs idle */
+	.secs_since_valid_hangup	= 10, /* hangup after secs idle */
+
+	.jitter_percent			= 20,
+};
+
+/*
+ * Scheduled sul callback that starts the connection attempt
+ */
+
+static void
+connect_client(lws_sorted_usec_list_t *sul)
+{
+	struct my_conn *m = lws_container_of(sul, struct my_conn, sul);
+	struct lws_client_connect_info i;
+
+	memset(&i, 0, sizeof(i));
+
+	i.context = context;
+	i.port = port;
+	i.address = server_address;
+	i.path = "/";
+	i.host = i.address;
+	i.origin = i.address;
+	i.ssl_connection = ssl_connection;
+	i.protocol = pro;
+	i.local_protocol_name = "lws-minimal-client";
+	i.pwsi = &m->wsi;
+	i.retry_and_idle_policy = &retry;
+	i.userdata = m;
+
+	if (!lws_client_connect_via_info(&i))
+		/*
+		 * Failed... schedule a retry... we can't use the _retry_wsi()
+		 * convenience wrapper api here because no valid wsi at this
+		 * point.
+		 */
+		if (lws_retry_sul_schedule(context, 0, sul, &retry,
+					   connect_client, &m->retry_count)) {
+			lwsl_err("%s: connection attempts exhausted\n", __func__);
+			interrupted = 1;
+		}
+}
+
+static int
+callback_minimal(struct lws *wsi, enum lws_callback_reasons reason,
+		 void *user, void *in, size_t len)
+{
+	struct my_conn *m = (struct my_conn *)user;
+
+	switch (reason) {
+
+	case LWS_CALLBACK_CLIENT_CONNECTION_ERROR:
+		lwsl_err("CLIENT_CONNECTION_ERROR: %s\n",
+			 in ? (char *)in : "(null)");
+		goto do_retry;
+		break;
+
+	case LWS_CALLBACK_CLIENT_RECEIVE:
+		lwsl_hexdump_notice(in, len);
+		break;
+
+	case LWS_CALLBACK_CLIENT_ESTABLISHED:
+		lwsl_user("%s: established\n", __func__);
+		break;
+
+	case LWS_CALLBACK_CLIENT_CLOSED:
+		goto do_retry;
+
+	default:
+		break;
+	}
+
+	return lws_callback_http_dummy(wsi, reason, user, in, len);
+
+do_retry:
+	/*
+	 * retry the connection to keep it nailed up
+	 *
+	 * For this example, we try to conceal any problem for one set of
+	 * backoff retries and then exit the app.
+	 *
+	 * If you set retry.conceal_count to be larger than the number of
+	 * elements in the backoff table, it will never give up and keep
+	 * retrying at the last backoff delay plus the random jitter amount.
+	 */
+	if (lws_retry_sul_schedule_retry_wsi(wsi, &m->sul, connect_client,
+					     &m->retry_count)) {
+		lwsl_err("%s: connection attempts exhausted\n", __func__);
+		interrupted = 1;
+	}
+
+	return 0;
+}
+
+static const struct lws_protocols protocols[] = {
+	{ "lws-minimal-client", callback_minimal, 0, 0, 0, NULL, 0 },
+	LWS_PROTOCOL_LIST_TERM
+};
+
+static void
+sigint_handler(int sig)
+{
+	interrupted = 1;
+}
+
+
 
 //Função principal --> Une todas as funções elaboradas e implementadas anteriormente.
 
-int main (){
+int main (int argc, const char **argv){
 
+    //SERVER
+    {
+	struct lws_context_creation_info info;
+	struct lws_context *context;
+	const char *p;
+	int n = 0, logs = LLL_USER | LLL_ERR | LLL_WARN | LLL_NOTICE
+			/* for LLL_ verbosity above NOTICE to be built into lws,
+			 * lws must have been configured and built with
+			 * -DCMAKE_BUILD_TYPE=DEBUG instead of =RELEASE */
+			/* | LLL_INFO */ /* | LLL_PARSER */ /* | LLL_HEADER */
+			/* | LLL_EXT */ /* | LLL_CLIENT */ /* | LLL_LATENCY */
+			/* | LLL_DEBUG */;
+
+	signal(SIGINT, sigint_handler);
+
+	if ((p = lws_cmdline_option(argc, argv, "-d")))
+		logs = atoi(p);
+
+	lws_set_log_level(logs, NULL);
+	lwsl_user("LWS minimal ws server | visit http://localhost:7681 (-s = use TLS / https)\n");
+
+	memset(&info, 0, sizeof info); /* otherwise uninitialized garbage */
+	info.port = 7681;
+	info.mounts = &mount;
+	info.protocols = protocols;
+	info.vhost_name = "localhost";
+#if defined(LWS_WITH_PLUGINS)
+	info.pvo = &pvo;
+#endif
+	info.options =
+		LWS_SERVER_OPTION_HTTP_HEADERS_SECURITY_BEST_PRACTICES_ENFORCE;
+
+#if defined(LWS_WITH_TLS)
+	if (lws_cmdline_option(argc, argv, "-s")) {
+		lwsl_user("Server using TLS\n");
+		info.options |= LWS_SERVER_OPTION_DO_SSL_GLOBAL_INIT;
+		info.ssl_cert_filepath = "localhost-100y.cert";
+		info.ssl_private_key_filepath = "localhost-100y.key";
+	}
+#endif
+
+	if (lws_cmdline_option(argc, argv, "-h"))
+		info.options |= LWS_SERVER_OPTION_VHOST_UPG_STRICT_HOST_CHECK;
+
+	if (lws_cmdline_option(argc, argv, "-v"))
+		info.retry_and_idle_policy = &retry;
+
+	context = lws_create_context(&info);
+	if (!context) {
+		lwsl_err("lws init failed\n");
+		return 1;
+	}
+
+	while (n >= 0 && !interrupted)
+		n = lws_service(context, 0);
+
+	lws_context_destroy(context);
+
+    //CLIENTE
+
+    struct lws_context_creation_info info;
+	const char *p;
+	int n = 0;
+
+	signal(SIGINT, sigint_handler);
+	memset(&info, 0, sizeof info);
+	lws_cmdline_option_handle_builtin(argc, argv, &info);
+
+	lwsl_user("LWS minimal ws client\n");
+
+	info.options = LWS_SERVER_OPTION_DO_SSL_GLOBAL_INIT;
+	info.port = CONTEXT_PORT_NO_LISTEN; /* we do not run any server */
+	info.protocols = protocols;
+
+#if defined(LWS_WITH_MBEDTLS) || defined(USE_WOLFSSL)
+	/*
+	 * OpenSSL uses the system trust store.  mbedTLS has to be told which
+	 * CA to trust explicitly.
+	 */
+	info.client_ssl_ca_filepath = "./libwebsockets.org.cer";
+#endif
+
+	if ((p = lws_cmdline_option(argc, argv, "--protocol")))
+		pro = p;
+
+	if ((p = lws_cmdline_option(argc, argv, "-s")))
+		server_address = p;
+
+	if ((p = lws_cmdline_option(argc, argv, "-p")))
+		port = atoi(p);
+
+	if (lws_cmdline_option(argc, argv, "-n"))
+		ssl_connection &= ~LCCSCF_USE_SSL;
+
+	if (lws_cmdline_option(argc, argv, "-j"))
+		ssl_connection |= LCCSCF_ALLOW_SELFSIGNED;
+
+	if (lws_cmdline_option(argc, argv, "-k"))
+		ssl_connection |= LCCSCF_ALLOW_INSECURE;
+
+	if (lws_cmdline_option(argc, argv, "-m"))
+		ssl_connection |= LCCSCF_SKIP_SERVER_CERT_HOSTNAME_CHECK;
+
+	if (lws_cmdline_option(argc, argv, "-e"))
+		ssl_connection |= LCCSCF_ALLOW_EXPIRED;
+
+	info.fd_limit_per_thread = 1 + 1 + 1;
+
+	context = lws_create_context(&info);
+	if (!context) {
+		lwsl_err("lws init failed\n");
+		return 1;
+	}
+
+	/* schedule the first client connection attempt to happen immediately */
+	lws_sul_schedule(context, 0, &mco.sul, connect_client, 1);
+
+	while (n >= 0 && !interrupted)
+		n = lws_service(context, 0);
+
+	lws_context_destroy(context);
+	lwsl_user("Completed\n");
+
+    //JOGO
     FILE* Arq_Lg;
     FILE* Arq_Pw;
     int TodasasSalas[5] = {0,1,2,3,4};
